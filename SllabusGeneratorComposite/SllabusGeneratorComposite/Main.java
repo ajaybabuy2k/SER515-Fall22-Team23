e4fd3a0bee711772826b988992baa2bab597fa49
rorugan2 <112654462+rorugan2@users.noreply.github.com>
2022-10-13 02:43:12 -0700

    Update Main.java

--- SllabusGeneratorComposite/SllabusGeneratorComposite/Main.java
+++ SllabusGeneratorComposite/SllabusGeneratorComposite/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
-//	private Facade facade;
+	private Facade facade;
 
 	public static void main(String[] args) {
-//	Admin a = new admin();
-//	a.createSyllabus();
-//	a.getSyllabus();
+	Admin a = new Admin();
+	a.createSyllabus();
+	a.getSyllabus();
 
 	}
 
